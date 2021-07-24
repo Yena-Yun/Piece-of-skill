896320482d5488491912a8845c94de0a8a720c4b
Yena-Yun <cheryleduart@gmail.com>
2021-07-24 17:15:21 +0900

    검색창 자동완성기능 구현해보기

---
+++ autocomplete/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    data: [
+      { name: "Andrew R. Kelly", age: 22, position: "Janitor" },
+      { name: "Adrian Sanchez", age: 30, position: "Teacher" },
+      { name: "Anderson Brown", age: 25, position: "Principal" },
+      { name: "Anna Valio", age: 30, position: "guidance councelor" },
+      { name: "Asha Mathews", age: 50, position: "Teacher" },
+      { name: "Alicia keys", age: 25, position: "Librarian" },
+      { name: "Alexa Dot", age: 30, position: "teacher" },
+      { name: "Bob Squarepants", age: 20, position: "secretary" },
+      { keyword: "" },
+      { results: [] }
+    ]
+  };
+
+  updateField = (field, value) => {
+    this.setState({ [field]: value });
+  };
+  
+  render() {
+    let { data } = this.state;
+    
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
