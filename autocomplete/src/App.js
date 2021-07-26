6fd46c448e4a44e72f9a110f0e94de858f5f3cce
Yena-Yun <cheryleduart@gmail.com>
2021-07-26 15:24:37 +0900

    검색창 자동완성 완료

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -1,47 +1,54 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
+import SearchBar from "./SearchBar";
+import "./App.css";
 
+import React, { Component } from "react";
 
 class App extends Component {
   state = {
     data: [
-      { name: "Andrew R. Kelly", age: 22, position: "Janitor" },
-      { name: "Adrian Sanchez", age: 30, position: "Teacher" },
-      { name: "Anderson Brown", age: 25, position: "Principal" },
-      { name: "Anna Valio", age: 30, position: "guidance councelor" },
-      { name: "Asha Mathews", age: 50, position: "Teacher" },
-      { name: "Alicia keys", age: 25, position: "Librarian" },
-      { name: "Alexa Dot", age: 30, position: "teacher" },
-      { name: "Bob Squarepants", age: 20, position: "secretary" },
+      { name: "React" },
+      { name: "Go" },
+      { name: "Java" },
+      { name: "Javascript" },
+      { name: "Python" },
+      { name: "React native" },
+      { name: "Node.js" },
+      { name: "Vue" }
     ],
     keyword: "",
     results: []
   };
 
+  matchName = (name, keyword) => {
+    console.log(name);
+    console.log(keyword);
+    var keyLen = keyword.length;
+    console.log(keyLen);
+
+    name = name.toLowerCase().substring(0, keyLen);
+    console.log(name);
+
+    if (keyword === "") return false;
+
+    return name === keyword;
+  };
+
+  onSearch = text => {
+    // console.log(text);
+
+    let data = this.state.data;
+    // console.log(data);
+
+    var results = data.filter(item => true === this.matchName(item.name, text));
+    // console.log(results);
+
+    this.setState({ results });
+  };
+
   updateField = (field, value) => {
+    console.log(field, value);
     this.onSearch(value);
     this.setState({ [field]: value });
-  };
-
-  // 찾으려는 값의 일부(keyword)만 입력해도 인식하는 함수
-  // 예:"Andrea".substring(0,3)은 "And"를 반환
-  // 대소문자 구분없이 인식하기 위해 모든 문자를 소문자로 설정
-  matchName = (name, keyword) => {
-    //입력한 값의 길이
-    var keyLength = keyword.length;
-    //입력한 값의 길이만큼 state의 name을 자름
-    name = name.toLowerCase().substring(0, keyLength);
-    //name에서 자른 부분과 입력한 값이 일치하고 입력한 값이 존재할 때 true 반환
-    return name === keyword && keyLength !== 0;
-  };
-
-  onSearch = (text) => {
-    let { data } = this.state;
-    //text(입력한 값)과 data의 name이 일치하는 경우만 배열로 반환하여
-    //state의 results 배열에 담는다 (= 자동완성목록)
-    var results = data.filter(item => this.matchName(item.name, text));
-    console.log(results);
-    this.setState({ results });
   };
   
   render() {
--- autocomplete/src/SearchBar.js
+++ autocomplete/src/SearchBar.js
@@ -1,33 +1,67 @@
-import React from 'react';
+import React from "react";
 import "./css/SearchBar.css";
 
-
 const SearchBar = ({ results, keyword, updateField }) => {
+  console.log(results);
+  console.log(keyword);
+  console.log(updateField);
 
+  //input창에 사용자가 클릭한 항목의 text를 넣어주고 preview 항목을 지우는 함수
+  var updateText = text => {
+    console.log(text);
+    updateField("keyword", text);
+    updateField("results", []);
+  };
 
-		return(
-			<div className="auto">
-				<input
-          className="search-bar"
-          placeholder="Search"
-          value={keyword}
-          onChange={(e) => updateField("keyword", e.target.value)}
-        />
+  var cancelSearch = () => {
+    updateField("keyword", "");
+  };
 
-        <div
-          onClick={() => this.updateText(keyword)}
-          className={`search-preview ${results.index === 0 ? "start" : ""}`}
-        >
-          <div className="first">
-            <p className="position">{results.position}</p>
-          </div>
+  var renderResults = results.map(({ name }, index) => {
+    return (
+      <SearchPreview
+        key={index}
+        updateText={updateText}
+        index={index}
+        name={name}
+      />
+    );
+  });
 
-          <div className="second">
-            <p className="age">{results.age}</p>
-          </div>
-        </div>
-			</div>
-		);
+  return (
+    <div className="auto">
+      <button
+        onClick={() => cancelSearch()}
+        className={`cancel-btn ${keyword.length > 0 ? "active" : "inactive"}`}
+      >
+        x
+      </button>
+      <input
+        className="search-bar"
+        placeholder="Search"
+        value={keyword}
+        onChange={e => updateField("keyword", e.target.value)}
+      />
+
+      {results.length > 0 ? (
+        <div className="search-results">{renderResults}</div>
+      ) : null}
+    </div>
+  );
+};
+
+//stateless component to render preview results
+const SearchPreview = ({ name, index, updateText }) => {
+  return (
+    <div
+      onClick={() => updateText(name)}
+      className={`search-preview ${index === 0 ? "start" : ""}`}
+    >
+      <div className="first">
+        <p className="name">{name}</p>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
