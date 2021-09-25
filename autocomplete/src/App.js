527542e7ca16156f5e65c8d824c6bf5f7661c901
Yena-Yun <cheryleduart@gmail.com>
2021-09-25 12:57:38 +0900

    autocomplete 복습 및 position으로 검색하는 기능 추가

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -1,62 +1,82 @@
-import SearchBar from "./SearchBar";
-import "./App.css";
+import SearchBar from './SearchBar';
+import './App.css';
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
   state = {
     data: [
-      { name: "React" },
-      { name: "Go" },
-      { name: "Java" },
-      { name: "Javascript" },
-      { name: "Python" },
-      { name: "React native" },
-      { name: "Node.js" },
-      { name: "Vue" }
+      { name: 'Andrew R. Kelly', age: 22, position: 'Janitor' },
+      { name: 'Adrian Sanchez', age: 30, position: 'Teacher' },
+      { name: 'Anderson Brown', age: 25, position: 'Principal' },
+      { name: 'Anna Valio', age: 30, position: 'guidance councelor' },
+      { name: 'Asha Mathews', age: 50, position: 'Teacher' },
+      { name: 'Alicia keys', age: 25, position: 'Librarian' },
+      { name: 'Alexa Dot', age: 30, position: 'teacher' },
+      { name: 'Bob Squarepants', age: 20, position: 'secretary' },
     ],
-    keyword: "",
-    results: []
+    // keyword = 인풋창 안의 내용
+    keyword: '',
+    // results = 자동완성 preview항목 내용
+    results: [],
   };
 
+  // 2. item의 name 중에 keyword를 포함하는 item이 있는지 확인하는 기능
   matchName = (name, keyword) => {
-    console.log(name);
-    console.log(keyword);
     var keyLen = keyword.length;
-    console.log(keyLen);
 
     name = name.toLowerCase().substring(0, keyLen);
-    console.log(name);
 
-    if (keyword === "") return false;
+    if (keyword === '') return false;
 
     return name === keyword;
   };
 
-  onSearch = text => {
+  matchPosition = (position, keyword) => {
+    var keyLen = keyword.length;
+
+    position = position.toLowerCase().substring(0, keyLen);
+
+    if (keyword === '') return false;
+
+    return position === keyword;
+  };
+
+  // 3. match 함수로 거른 item을 state에 넣어주는 역할만 하는 기능
+  onSearch = (text) => {
     let data = this.state.data;
 
-    var results = data.filter(item => true === this.matchName(item.name, text));
+    // (걸러낸 item을 담는 변수 이름이 state의 key 이름(results)과 똑같아야 함 - 변수를 resultArry 등으로 바꾸면 자동완성 항목이 나타나지 않음)
+    var results = data.filter((item) => true === this.matchName(item.name, text) || true === this.matchPosition(item.position, text));
+    // console.log('matchName으로 걸러진 item들 배열:' + results);
+
+    // { results }: matchName으로 걸러진 item 배열을 'results'라는 객체에 넣음 (콘솔에 {results: Array(2)}라고 찍힘 - 안에 요소가 2개 들어간 배열을 가진 results라는 이름의 객체)
+    console.log({ results }); // (한 마디로 '객체'화)
 
     this.setState({ results });
   };
 
+  // 1. 사용자한테 입력된 값을 'keyword' 키(= field)로 받음
   updateField = (field, value) => {
-    console.log(field, value);
+    console.log('updateField 함수:' + field); // 입력되는 동안 계속 'keyword'로 찍힘
+    console.log('updateField 함수:' + value); // 인풋창에 입력되고 있는 값 그대로 찍힘
+
+    // onSearch 함수에 입력된 값(= keyword)을 넣어주고
     this.onSearch(value);
+
+    // setState의 'keyword' 키로 받아온 입력값(value)이 들어감
     this.setState({ [field]: value });
+
+    // (주의: 항목 중 하나를 '클릭'한 경우에는 updateField가 아닌 'updateText' 함수가 실행 => field에 'keyword' 키로 클릭한 항목의 내용, 'result' 키로 빈 배열 넣음)
   };
-  
+
   render() {
-    let { results, keyword } = this.state;
+    // SearchBar에서 updateField 함수 처리를 할 수 있게 keyword와 results 키의 value 자리를 가져옴
+    let { results, keyword } = this.state; // (비구조화 할당)
 
     return (
-      <div className="App">
-        <SearchBar
-          results={results}
-          keyword={keyword}
-          updateField={this.updateField}
-        />
+      <div className='App'>
+        <SearchBar results={results} keyword={keyword} updateField={this.updateField} />
       </div>
     );
   }
--- autocomplete/src/SearchBar.js
+++ autocomplete/src/SearchBar.js
@@ -1,59 +1,68 @@
-import React from "react";
-import "./css/SearchBar.css";
+import React from 'react';
+import './css/SearchBar.css';
 
 const SearchBar = ({ results, keyword, updateField }) => {
+  // results: onChange로 updateField 함수가 실행될 때마다 filter된 item들을 보여줄 배열 (= 자동완성 Preview 항목에 보여질 내용)
+  console.log(results);
 
-  //input창에 사용자가 클릭한 항목의 text를 넣어주고 preview 항목을 지우는 함수
-  var updateText = text => {
-    updateField("keyword", text);
-    updateField("results", []);
-  };
-
+  // 삭제버튼 클릭 시
   var cancelSearch = () => {
-    updateField("keyword", "");
+    // keyword 자리(= 입력창)의 값 초기화
+    updateField('keyword', '');
   };
 
-  var renderResults = results.map(({ name }, index) => {
-    return (
-      <SearchPreview
-        key={index}
-        updateText={updateText}
-        index={index}
-        name={name}
-      />
-    );
-  });
+  // updateText: Preview에서 선택(클릭)한 항목이 있을 때만 실행되는 함수!!
+  var updateText = (text) => {
+    // state의 keyword 키의 value로 입력한 값(text)을 넣음
+    //  => 클릭한 항목의 내용을 입력창 안에 보여줌
+    updateField('keyword', text);
+    // state의 results 키의 value로 빈 배열 업데이트
+    //  => 사용자가 특정 항목을 선택하고 나면 preview의 항목 내용을 삭제
+    updateField('results', []);
+  };
 
   return (
-    <div className="auto">
-      <button
-        onClick={() => cancelSearch()}
-        className={`cancel-btn ${keyword.length > 0 ? "active" : "inactive"}`}
-      >
+    <div className='auto'>
+      {/* x 버튼 클릭 시 인풋창 내용 삭제 */}
+      <button onClick={() => cancelSearch()} className={`cancel-btn ${keyword.length > 0 ? 'active' : 'inactive'}`}>
         x
       </button>
-      <input
-        className="search-bar"
-        placeholder="Search"
-        value={keyword}
-        onChange={e => updateField("keyword", e.target.value)}
-      />
 
-      {results.length > 0 ? (
-        <div className="search-results">{renderResults}</div>
+      {/* 사용자가 입력을 시작하면 바로 updateField 함수에 field로 'keyword' 키, value로 입력된 값 e.target.value 들어감 */}
+      <input className='search-bar' placeholder='Search' value={keyword} onChange={(e) => updateField('keyword', e.target.value)} />
+
+      {/* results 배열이 비어있지 않으면 -> Preview 항목 렌더링 */}
+      {results.length > 0 ? ( // 일반소괄호 = return
+        <div className='search-results'>
+          {/* renderResults: results 배열에서 name과 index를 꺼내서 Preview에 전달 반환 - 꺼냄 (key값을 통해 객체로 꺼냄) */}
+          {results.map(({ name, position }, index) => {
+            console.log(name); // Andrew R. Kelly
+            console.log({ name }); // {name: 'Andrew R. Kelly'}
+
+            return <SearchPreview key={index} updateText={updateText} index={index} name={name} position={position} />;
+          })}
+        </div>
       ) : null}
     </div>
   );
 };
 
-const SearchPreview = ({ name, index, updateText }) => {
+// Preview 항목 보여주는 함수
+const SearchPreview = ({ updateText, index, name, position }) => {
   return (
+    // preview 중 선택(클릭)한 항목에 대해 updateText 함수 실행
     <div
-      onClick={() => updateText(name)}
-      className={`search-preview ${index === 0 ? "start" : ""}`}
+      onClick={() => {
+        updateText(name);
+        updateText(position);
+      }}
+      className={`search-preview ${index === 0 ? 'start' : ''}`}
     >
-      <div className="first">
-        <p className="name">{name}</p>
+      <div className='first'>
+        <p className='name'>{name}</p>
+      </div>
+      <div className='second'>
+        <p className='position'>{position}</p>
       </div>
     </div>
   );
