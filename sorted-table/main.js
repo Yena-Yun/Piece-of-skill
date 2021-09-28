e459ce051b81db625b6c3f5a68786d49faa875f0
Yena-Yun <cheryleduart@gmail.com>
2021-09-28 17:49:55 +0900

    헤더누르면 정렬되는 표

--- sorted-table/main.js
+++ sorted-table/main.js
@@ -1,55 +1,180 @@
-window.addEventListener('DOMContentLoaded', () => {
-  let dir = 'dsc';
-  th = document.getElementsByTagName('th');
-  for (let c = 0; c < th.length; c++) {
-    th[c].addEventListener('click', item(c));
-  }
-  function item(c) {
-    return function () {
-      if (dir == 'asc') {
-        dir = 'dsc';
-      } else {
-        dir = 'asc';
-      }
-      sortTable(c, dir);
-    };
-  }
-  function sortTable(c, sort_dir) {
-    let table_rows, switching, i, x, y, shouldSwitch;
-    let table = document.getElementsByClassName('results_table');
-    switching = true;
-    table_rows = table[0].rows;
-    while (switching) {
-      // Start by saying: no switching is done:
-      switching = false;
-      for (i = 0; i < table_rows.length - 1; i++) {
-        shouldSwitch = false;
-        let present_row = table_rows[i];
-        let next_row = table_rows[i + 1];
+function sortingNumber(a, b) {
+  if (typeof a == 'number' && typeof b == 'number') return a - b;
 
-        let present_percentage = present_row.cells[1].getElementsByClassName('row_values')[0].cells[1].innerText;
-        let next_percentage = next_row.cells[1].getElementsByClassName('row_values')[0].cells[1].innerText;
-        let present_percentage_length = present_percentage.length;
-        present_percentage = present_percentage.substring(0, present_percentage.length - 1);
-        let next_percentage_length = next_percentage.length;
-        next_percentage = next_percentage.substring(0, next_percentage.length - 1);
-        if (sort_dir == 'asc') {
-          if (parseInt(present_percentage, 10) > parseInt(next_percentage, 10)) {
-            shouldSwitch = true;
-            break;
-          }
-        }
-        if (sort_dir == 'dsc') {
-          if (parseInt(present_percentage, 10) < parseInt(next_percentage, 10)) {
-            shouldSwitch = true;
-            break;
-          }
-        }
-      }
-      if (shouldSwitch) {
-        table_rows[i].parentNode.insertBefore(table_rows[i + 1], table_rows[i]);
-        switching = true;
-      }
-    }
+  // 천단위 쉼표와 공백문자만 삭제하기.
+  var a = (a + '').replace(/[,\s\xA0]+/g, '');
+  var b = (b + '').replace(/[,\s\xA0]+/g, '');
+
+  var numA = parseFloat(a) + '';
+  var numB = parseFloat(b) + '';
+
+  if (numA == 'NaN' || numB == 'NaN' || a != numA || b != numB) return false;
+
+  return parseFloat(a) - parseFloat(b);
+}
+
+/* changeForSorting() : 문자열 바꾸기. */
+
+function changeForSorting(first, second) {
+  // 문자열의 복사본 만들기.
+  var a = first.toString().replace(/[\s\xA0]+/g, ' ');
+  var b = second.toString().replace(/[\s\xA0]+/g, ' ');
+
+  var change = { first: a, second: b };
+
+  if (a.search(/\d/) < 0 || b.search(/\d/) < 0 || a.length == 0 || b.length == 0) return change;
+
+  var regExp = /(\d),(\d)/g; // 천단위 쉼표를 찾기 위한 정규식.
+
+  a = a.replace(regExp, '$1' + '$2');
+  b = b.replace(regExp, '$1' + '$2');
+
+  var unit = 0;
+  var aNb = a + ' ' + b;
+  var numbers = aNb.match(/\d+/g); // 문자열에 들어있는 숫자 찾기
+
+  for (var x = 0; x < numbers.length; x++) {
+    var length = numbers[x].length;
+    if (unit < length) unit = length;
   }
-});
+
+  var addZero = function (string) {
+    // 숫자들의 단위 맞추기
+
+    var match = string.match(/^0+/);
+
+    if (string.length == unit) return match == null ? string : match + string;
+
+    var zero = '0';
+
+    for (var x = string.length; x < unit; x++) string = zero + string;
+
+    return match == null ? string : match + string;
+  };
+
+  change.first = a.replace(/\d+/g, addZero);
+  change.second = b.replace(/\d+/g, addZero);
+
+  return change;
+}
+
+/* byLocale() */
+
+function byLocale() {
+  var compare = function (a, b) {
+    var sorting = sortingNumber(a, b);
+
+    if (typeof sorting == 'number') return sorting;
+
+    var change = changeForSorting(a, b);
+
+    var a = change.first;
+    var b = change.second;
+
+    return a.localeCompare(b);
+  };
+
+  var ascendingOrder = function (a, b) {
+    return compare(a, b);
+  };
+  var descendingOrder = function (a, b) {
+    return compare(b, a);
+  };
+
+  return { ascending: ascendingOrder, descending: descendingOrder };
+}
+
+/* replacement() */
+
+function replacement(parent) {
+  var tagName = parent.tagName.toLowerCase();
+  if (tagName == 'table') parent = parent.tBodies[0];
+  tagName = parent.tagName.toLowerCase();
+  if (tagName == 'tbody') var children = parent.rows;
+  else var children = parent.getElementsByTagName('li');
+
+  var replace = {
+    order: byLocale(),
+    index: false,
+    array: (function () {
+      var array = [];
+      for (var x = 0; x < children.length; x++) array[x] = children[x];
+      return array;
+    })(),
+    checkIndex: function (index) {
+      if (index) this.index = parseInt(index, 10);
+      var tagName = parent.tagName.toLowerCase();
+      if (tagName == 'tbody' && !index) this.index = 0;
+    },
+    getText: function (child) {
+      if (this.index) child = child.cells[this.index];
+      return getTextByClone(child);
+    },
+    setChildren: function () {
+      var array = this.array;
+      while (parent.hasChildNodes()) parent.removeChild(parent.firstChild);
+      for (var x = 0; x < array.length; x++) parent.appendChild(array[x]);
+    },
+    ascending: function (index) {
+      // 오름차순
+      this.checkIndex(index);
+      var _self = this;
+      var order = this.order;
+      var ascending = function (a, b) {
+        var a = _self.getText(a);
+        var b = _self.getText(b);
+        return order.ascending(a, b);
+      };
+      this.array.sort(ascending);
+      this.setChildren();
+    },
+    descending: function (index) {
+      // 내림차순
+      this.checkIndex(index);
+      var _self = this;
+      var order = this.order;
+      var descending = function (a, b) {
+        var a = _self.getText(a);
+        var b = _self.getText(b);
+        return order.descending(a, b);
+      };
+      this.array.sort(descending);
+      this.setChildren();
+    },
+  };
+  return replace;
+}
+
+function getTextByClone(tag) {
+  var clone = tag.cloneNode(true); // 태그의 복사본 만들기.
+  var br = clone.getElementsByTagName('br');
+  while (br[0]) {
+    var blank = document.createTextNode(' ');
+    clone.insertBefore(blank, br[0]);
+    clone.removeChild(br[0]);
+  }
+  var isBlock = function (tag) {
+    var display = '';
+    if (window.getComputedStyle) display = window.getComputedStyle(tag, '')['display'];
+    else display = tag.currentStyle['display'];
+    return display == 'block' ? true : false;
+  };
+  var children = clone.getElementsByTagName('*');
+  for (var x = 0; x < children.length; x++) {
+    var child = children[x];
+    if (!('value' in child) && isBlock(child)) child.innerHTML = child.innerHTML + ' ';
+  }
+  var textContent = 'textContent' in clone ? clone.textContent : clone.innerText;
+  return textContent;
+}
+
+var myTable = document.getElementById('KoreanMovie');
+var replace = replacement(myTable);
+
+function sortTD(index) {
+  replace.ascending(index);
+}
+
+function reverseTD(index) {
+  replace.descending(index);
+}
