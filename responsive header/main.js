c09d309828887cdc4fc8ac9c2fed01e849203fe4
Yena-Yun <cheryleduart@gmail.com>
2022-02-24 11:10:52 +0900

    폴더정리중: 일부 폴더 삭제

--- responsive header/main.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.querySelector(".navbar-toggle-btn");
-const navBtns = document.querySelector(".nav-btns");
-const shareLinks = document.querySelector(".share-links");
-
-toggleBtn.addEventListener("click", () => {
-	// classList.toggle("클래스명"): 클릭 시 active가 없으면 추가, 있으면 제거
-	navBtns.classList.toggle("active");
-	shareLinks.classList.toggle("active");
-});
